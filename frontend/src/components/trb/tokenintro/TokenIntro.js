d0b58a0f3c2d0d761bdcecdae5c544e4f5322e26
Wallace Palmer <wallace.m.palmer@gmail.com>
2021-10-19 11:26:09 -0700

    added TokenIntro

--- frontend/src/components/trb/tokenintro/TokenIntro.js
+++ frontend/src/components/trb/tokenintro/TokenIntro.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './TokenIntro.scss';
 
 const TokenIntro = () => {
-    return (
-        <div className="viewContainer">
-
-        </div>
-    )
+  return (
+    <div className="TokenIntro">
+      <h1>TRB, Tellor’s native token</h1>
+      <div className="TokenIntro__desc">
+        <p>The first way to get involved with Tellor is acquiring some TRB.</p>
+        <p>The purpose of Tellor’s native token, Tributes (also known as TRB), is to connect and align data reporters, oracle users, community governance and treasury decisions.</p>
+      </div>
+    </div>
+  )
 }
 
 export default TokenIntro;
