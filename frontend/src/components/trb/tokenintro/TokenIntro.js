44ffd5a45965788426145de3227d0126892414c3
Wallace Palmer <wallace.m.palmer@gmail.com>
2021-10-19 13:20:47 -0700

    added HeaderDesc component

--- frontend/src/components/trb/tokenintro/TokenIntro.js
+++ frontend/src/components/trb/tokenintro/TokenIntro.js
@@ -4,10 +4,10 @@ import './TokenIntro.scss';
 const TokenIntro = () => {
   return (
     <div className="TokenIntro">
-      <h1>TRB, Tellor’s native token</h1>
+      <h1 className="page-header">TRB, Tellor’s native token</h1>
       <div className="TokenIntro__desc">
-        <p>The first way to get involved with Tellor is acquiring some TRB.</p>
-        <p>The purpose of Tellor’s native token, Tributes (also known as TRB), is to connect and align data reporters, oracle users, community governance and treasury decisions.</p>
+        <p className="page-text">The first way to get involved with Tellor is acquiring some TRB.</p>
+        <p className="page-text">The purpose of Tellor’s native token, Tributes (also known as TRB), is to connect and align data reporters, oracle users, community governance and treasury decisions.</p>
       </div>
     </div>
   )
