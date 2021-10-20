1fae2a66c0810bb2a08bf4ae4f5b1b44f4056c49
Wallace Palmer <wallace.m.palmer@gmail.com>
2021-10-20 15:01:58 -0700

    added BountiesWrapper footer text

--- frontend/src/components/shared/Bounties/BountiesWrapper.js
+++ frontend/src/components/shared/Bounties/BountiesWrapper.js
@@ -12,6 +12,11 @@ export default function BountiesWrapper() {
                 <h1>, grab a bounty!</h1>
             </div>
             <Bounties />
+            <div className="BountiesWrapper__footer">
+                <p>The Tellor Bounties program is a way to reward developers who help us build out Tellor.
+    We have a dev fund that's for developing Tellor and we plan to use it!</p>
+                <a href="#">read ground rules</a>
+            </div>
         </div>
     )
 }
