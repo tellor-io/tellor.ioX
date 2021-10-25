cb39808b42cacf66d73fe30c49419bd985d039a7
Wallace Palmer <wallace.m.palmer@gmail.com>
2021-10-25 13:42:40 -0700

    added datapoints dropdown

--- frontend/src/components/requestnew/requestdatapoints/RequestDatapoints.js
+++ frontend/src/components/requestnew/requestdatapoints/RequestDatapoints.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './RequestDatapoints.scss'
 import Datapoint from '../../shared/Datapoint/Datapoint';
@@ -6,41 +6,100 @@ import Datapoint from '../../shared/Datapoint/Datapoint';
 
 const RequestDatapoints = () => {
 
-     // //// DUMMY ////
-     const dataPoints = [
-        {
-            timestamp: "1633959919",
-            reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
-            symbols: "ETH/USD",
-            value: "3957.33"
-        },
-        {
-            timestamp: "1633959564",
-            reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
-            symbols: "BTC/USD",
-            value: "43761.1"
-        },
-        {
-            timestamp: "1633959554",
-            reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
-            symbols: "BNB/USD",
-            value: "355.15"
-        },
-    ]
+    // //// DUMMY ////
+   const datapoints = [
+    {
+        timestamp: "1633959919",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "ETH/USD",
+        value: "3957.33"
+    },
+    {
+        timestamp: "1633959564",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "BTC/USD",
+        value: "43761.1"
+    },
+    {
+        timestamp: "1633959554",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "BNB/USD",
+        value: "355.15"
+    },
+    {
+        timestamp: "1633959919",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "ETH/USD",
+        value: "3957.33"
+    },
+    {
+        timestamp: "1633959564",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "BTC/USD",
+        value: "43761.1"
+    },
+    {
+        timestamp: "1633959554",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "BNB/USD",
+        value: "355.15"
+    },
+    {
+        timestamp: "1633959919",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "ETH/USD",
+        value: "3957.33"
+    },
+    {
+        timestamp: "1633959564",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "BTC/USD",
+        value: "43761.1"
+    },
+    {
+        timestamp: "1633959554",
+        reporter: "0x1D39955c9662678535d68a966862A06956ea5644",
+        symbols: "BNB/USD",
+        value: "355.15"
+    },
+]
 
-    const datapointstotal = 55;
+    const [open, setOpen] = useState(false);
+    const [datapointsTotal, setDatapointsTotal ] = useState(0)
+    const [linkText, setLinkText] = useState('');
+    
+    useEffect(() => {
+        setLinkText( open ? 'see less...' :  `see all (${datapointsTotal})`);
+        setDatapointsTotal(datapoints.length);
+    });
+
+    //toggle data point visibility 
+    const handleClick = () => {
+        setOpen(!open);
+    }
 
     return (
         <div className="RequestDatapoints">
             <div className="Datapoints">
                 <p className="popular-points">Most popular datapoints:</p>
-                {dataPoints.map((data,i) => {
-                    return <Datapoint key={i} data={data}/>
+                {datapoints.map((data,i) => {
+                    if (open) { 
+                        //only show all datapoints if open
+                        return <Datapoint key={i} data={data}/>
+                    } else if ( i < 3 && !open) {
+                        return <Datapoint key={i} data={data}/>
+                    }
                 })}
             </div>
-            <a href="" target="_blank" className="see-all"> see all (50)</a>
+            {/* only show dropdownf if more than 3 datapoints */}
+            { datapointsTotal > 3 && (
+                <a onClick={() => handleClick()} target="_blank" className="see-all">{linkText}</a>
+            )}
         </div>
     )
 }
+
+   
+
 
 export default RequestDatapoints;
