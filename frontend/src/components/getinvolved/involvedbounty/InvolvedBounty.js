6e094e202f80e10a30fb2643c7154a4be109ce97
Wallace Palmer <wallace.m.palmer@gmail.com>
2021-10-28 13:33:20 -0700

    set up routes and basic getinvolved components

---
+++ frontend/src/components/getinvolved/involvedbounty/InvolvedBounty.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './InvolvedBounty.scss'
+
+const InvolvedBounty = () => {    
+
+    return (
+        <div className="InvolvedBounty">
+
+        </div>
+    )
+}
+
+
+
+export default InvolvedBounty;
---
+++ frontend/src/components/getinvolved/involvedintro/InvolvedIntro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './InvolvedIntro.scss'
+
+const InvolvedIntro = () => {    
+
+    return (
+        <div className="InvolvedIntro">
+
+        </div>
+    )
+}
+
+
+
+export default InvolvedIntro;
