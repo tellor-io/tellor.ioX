67690922bbc61257097a2dfe4f201c3adc8c46b0
Wallace Palmer <wallace.m.palmer@gmail.com>
2021-11-02 09:49:47 -0700

    bounties dark theme finished

--- frontend/src/components/shared/Bounties/BountiesWrapper.js
+++ frontend/src/components/shared/Bounties/BountiesWrapper.js
@@ -13,23 +13,27 @@ export default function BountiesWrapper(props) {
         darkBackground
     } = props;
 
+    //dynamic className
+    const classType = darkBackground ? "BountiesWrapperDark" : "BountiesWrapper";
+
     return (
-        <div className={darkBackground ? "BountiesWrapperDark" : "BountiesWrapper"}>
+        <div className={classType}>
+            {/* show or hide header */}
             {!hideHeader && (
-                <div className="BountiesWrapper__title">
+                <div className={`${classType}__title`}>
                     <h1>Earn TRB</h1>
                     <Swoosh />
                     <h1>, grab a bounty!</h1>
                 </div>
             )}
-            <Bounties />
-            <div className="BountiesWrapper__desc">
+            <Bounties darkBackground={darkBackground} />
+            <div className={`${classType}__desc`}>
                 <p>The Tellor Bounties program is a way to reward developers who help us build out Tellor. We have a dev fund that's for developing Tellor and we plan to use it!</p>
                 <a onClick={() => setToggle(!toggle)}>read ground rules</a>
             </div>
             {/* collapsable area */}
             { toggle && (
-                <div className="BountiesWrapper__collapse">
+                <div className={`${classType}__collapse`}>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             )}
