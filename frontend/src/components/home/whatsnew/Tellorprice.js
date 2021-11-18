573979923e4ff5db223fd2a1b834102ffe5a8e97
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-11-18 11:10:31 -0700

    fixes mobile variables to dynamically pull from redux

--- frontend/src/components/home/whatsnew/Tellorprice.js
+++ frontend/src/components/home/whatsnew/Tellorprice.js
@@ -38,24 +38,23 @@ export default function Tellorprice() {
   return (
     <div ref={effectRef} className="Tellorprice">
       <Swoosh />
-        <div className="Tellorprice__txt">
-
-          <a
-            href="https://www.coingecko.com/en/coins/tellor"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tellor (TRB)
-          </a>
-          <p ref={priceRef}>
-            {prevPrice && prevPrice != 0
-              ? new Intl.NumberFormat("en-EN", {
-                  style: "currency",
-                  currency: "USD",
-                }).format(prevPrice)
-              : "Loading..."}
-          </p>
-        </div>
+      <div className="Tellorprice__txt">
+        <a
+          href="https://www.coingecko.com/en/coins/tellor"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tellor (TRB)
+        </a>
+        <p ref={priceRef}>
+          {prevPrice && prevPrice != 0
+            ? new Intl.NumberFormat("en-EN", {
+                style: "currency",
+                currency: "USD",
+              }).format(prevPrice)
+            : "Loading..."}
+        </p>
+      </div>
     </div>
   );
 }
