22bcbba4e1b019a6ff76fb6fbd04d88a55279648
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-11-29 09:03:41 -0700

    comments out function restructure to make data pull from graph correctly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -66,7 +66,7 @@ const App = (props) => {
     startTwitter();
     console.log("DATA IN USE EFFECT", data);
     startGetEvents(loading, error, data);
-  }, [currentNetwork]);
+  }, [currentNetwork, data]);
 
   useEffect(() => {
     if (votes && votes.disputes) {
