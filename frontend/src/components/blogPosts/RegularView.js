2d8eeacd6efe2adef0f3b24c96d95695d5a4f264
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-12-01 08:57:27 -0700

    adds error handling for website fixes

--- frontend/src/components/blogPosts/RegularView.js
+++ frontend/src/components/blogPosts/RegularView.js
@@ -13,35 +13,12 @@ function RegularView() {
   return (
     <div className="BlogPosts">
       <div className="topRow">
-      {blogContext.hero[0] &&
-        blogContext.hero.map((post) => {
-          return (
-            <Link
-              key={post.id}
-              className="hero"
-              to={{
-                pathname: `/blog/${post.postUrl}`,
-                state: {
-                  post: post,
-                },
-              }}
-            >
-              <img
-                src={"https://strapi.tellor.io" + post.blogImage.url}
-                alt={post.blogImage.name}
-              />
-              <h5>{post.publishDate}</h5>
-              <h1>{titleFormatter(post.title)}</h1>
-            </Link>
-          );
-        })}
-      <div className="subhero">
-        {blogContext.subhero[0] &&
-          blogContext.subhero.map((post) => {
+        {blogContext.hero[0] &&
+          blogContext.hero.map((post) => {
             return (
               <Link
                 key={post.id}
-                className="subhero__innerDivs"
+                className="hero"
                 to={{
                   pathname: `/blog/${post.postUrl}`,
                   state: {
@@ -58,7 +35,30 @@ function RegularView() {
               </Link>
             );
           })}
-      </div>
+        <div className="subhero">
+          {blogContext.subhero[0] &&
+            blogContext.subhero.map((post) => {
+              return (
+                <Link
+                  key={post.id}
+                  className="subhero__innerDivs"
+                  to={{
+                    pathname: `/blog/${post.postUrl}`,
+                    state: {
+                      post: post,
+                    },
+                  }}
+                >
+                  <img
+                    src={"https://strapi.tellor.io" + post.blogImage.url}
+                    alt={post.blogImage.name}
+                  />
+                  <h5>{post.publishDate}</h5>
+                  <h1>{titleFormatter(post.title)}</h1>
+                </Link>
+              );
+            })}
+        </div>
       </div>
       <div className="posts">
         {blogContext.posts[0] &&
--- frontend/src/components/home/whatsnew/HomeBlogpost.js
+++ frontend/src/components/home/whatsnew/HomeBlogpost.js
@@ -11,12 +11,18 @@ export default function HomeBlogpost() {
   return (
     <div className="HomeBlogpost" onClick={() => history.push("/blog")}>
       <div className="HomeBlogpost__image">
-        <img src={"https://strapi.tellor.io" + mostRecent.blogImage.url} />
+        <img
+          src={
+            mostRecent
+              ? "https://strapi.tellor.io" + mostRecent.blogImage.url
+              : ""
+          }
+        />
         {/* <img src="https://i.pinimg.com/564x/77/a8/c0/77a8c06b4c278d029b45bc73cb6666af.jpg" /> */}
       </div>
       <div className="HomeBlogpost__txt">
         <p>Blogpost</p>
-        <h4>{mostRecent.title}</h4>
+        <h4>{mostRecent ? mostRecent.title : ""}</h4>
       </div>
     </div>
   );
